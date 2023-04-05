4e6ba68a90a085fc348fc84cf584a7821d618e77
eunhye919 <masimel33@gmail.com>
2023-04-05 13:22:19 +0900

    문제 풀이

---
+++ b1920/app.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+// 1
+// 1
+// 0
+// 0
+// 1
+let [N, A, M, B] = input.map(v => v.split(" "));
+A = A.map(el => Number(el))
+B = B.map(el => Number(el))
+
+A.sort((a,b)=>a-b)
+let answer = [];
+
+B.forEach(v=>{
+  let left = 0;
+  let right = A.length-1; 
+  let find = false; 
+    while(left<=right){
+      let mid = Math.floor((right+left)/2);   
+      if(A[mid]>v){
+        right = mid-1; 
+      }else if(A[mid]<v){
+        left=mid+1;
+      }else{  
+        find=true;
+        break; 
+      }
+    }
+  
+	find ? answer.push(1) : answer.push(0)
+})
+console.log(answer.join('\n'))
+
+
+// let numSet = new Set(A)
+// let result = B.map(el => numSet.has(el) ? 1 : 0)
+// console.log(result.join('\n'))
+
+// for(let x of B) {
+// 	if(A.includes(x)) {
+// 		console.log(1)
+// 	}else {
+// 		console.log(0)
+// 	}
+// }
