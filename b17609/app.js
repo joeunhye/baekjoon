9af1339a2c8df1d3afa23071c7f8b2cc6f3b8aea
eunhye919 <masimel33@gmail.com>
2023-07-06 13:44:42 +0900

    문제풀이

---
+++ b17609/app.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+const isPalindrom = (input) => {
+	const n = input.length;
+	for(let i = 0; i < n / 2; i++) {
+		if(input[i] !== input[n - i - 1]) {
+			return false
+		}
+	}
+	return true;
+}
+
+const t = Number(input[0]);
+
+for(let i = 1; i <= t; i++) {
+	const data = input[i];
+
+	if(isPalindrom(data)) console.log(0) // 회문인 경우
+	else { // 회문이 아닌 경우, 유사 회문인지 판별
+		let flag = false;
+		const n = data.length;
+
+		for(let j = 0; j < n / 2 && !flag; j++) {
+			// 대칭이 아닌 경우
+			console.log(data.slice(0, j), data.slice(j + 1) )
+			if(data[j] !== data[n - j - 1]) {
+				if(isPalindrom(data.slice(0, j) + data.slice(j + 1))) flag = true;
+				if(isPalindrom(data.slice(0, n - j - 1) + data.slice(n - j))) flag = true;
+				break;
+			}
+		}
+		if(flag) console.log(1)
+		else console.log(2)
+	}
+}
+
+// https://kingseungil.tistory.com/164
+// https://breathtaking-life.tistory.com/167
--- b19939/app.js
+++ b19939/app.js
@@ -1,4 +1,15 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let [N, K] = fs.readFileSync(filePath).toString().split("\n");
-console.log(input)
+let [n, k] = fs.readFileSync(filePath).toString().split("\n")[0].split(' ').map(Number);
+
+let sum = k*(k+1)/2;
+
+if(sum > n) {console.log(-1) 
+}else {
+    n -= sum;
+    if(n % k == 0) console.log(k - 1)
+    else console.log(k)
+}
+
+//https://jaekwan.tistory.com/154
+//https://velog.io/@aurpo1/%EB%B0%B1%EC%A4%80-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-19939-%EB%B0%95-%ED%84%B0%EB%9C%A8%EB%A6%AC%EA%B8%B0
