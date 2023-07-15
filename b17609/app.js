3b94d983125ad1da7bd42559915af643a8a0a9f9
joeunhye <masimel33@gmail.com>
2023-07-15 20:33:41 +0900

    문제풀이

--- b17609/app.js
+++ b17609/app.js
@@ -2,37 +2,39 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-const isPalindrom = (input) => {
+const isPalindrom = input => {
 	const n = input.length;
-	for(let i = 0; i < n / 2; i++) {
-		if(input[i] !== input[n - i - 1]) {
-			return false
+	for (let i = 0; i < n / 2; i++) {
+		if (input[i] !== input[n - i - 1]) {
+			return false;
 		}
 	}
 	return true;
-}
+};
 
 const t = Number(input[0]);
 
-for(let i = 1; i <= t; i++) {
+for (let i = 1; i <= t; i++) {
 	const data = input[i];
 
-	if(isPalindrom(data)) console.log(0) // 회문인 경우
-	else { // 회문이 아닌 경우, 유사 회문인지 판별
+	if (isPalindrom(data)) console.log(0); // 회문인 경우
+	else {
+		// 회문이 아닌 경우, 유사 회문인지 판별
 		let flag = false;
 		const n = data.length;
 
-		for(let j = 0; j < n / 2 && !flag; j++) {
+		for (let j = 0; j < n / 2 && !flag; j++) {
 			// 대칭이 아닌 경우
-			// console.log(data.slice(0, j), data.slice(j + 1) )
-			if(data[j] !== data[n - j - 1]) {
-				if(isPalindrom(data.slice(0, j) + data.slice(j + 1))) flag = true;
-				if(isPalindrom(data.slice(0, n - j - 1) + data.slice(n - j))) flag = true;
+			if (data[j] !== data[n - j - 1]) {
+				// 왼쪽에 있는 원소를 제거한 뒤 회문 검사
+				if (isPalindrom(data.slice(0, j) + data.slice(j + 1))) flag = true;
+				// 오른쪽에 있는 원소를 제거한 뒤 회문 검사
+				if (isPalindrom(data.slice(0, n - j - 1) + data.slice(n - j))) flag = true;
 				break;
 			}
 		}
-		if(flag) console.log(1)
-		else console.log(2)
+		if (flag) console.log(1);
+		else console.log(2);
 	}
 }
 
